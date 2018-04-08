7eb25771de2d99474f306706dbb17e9ca902144a
Ubuntu <ubuntu@ip-172-31-44-106.us-east-2.compute.internal>
2018-04-08 00:03:33 +0000

    Первая версия

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,33 @@
+import os
+import sys
+
+
+def get_size(filepath):
+    return os.stat(filepath)[6]
+
+
+def get_doubles(root_dir):
+    files = []
+    doubles = []
+    for (dirpath, dirnames, filenames) in os.walk(root_dir):
+        try:
+            for filename in filenames:
+                fullpath = os.path.join(dirpath, filename)
+                file_stat = [filename, get_size(fullpath)]
+                if file_stat in files:
+                    if file_stat not in doubles:
+                        doubles.append(file_stat)
+                else:
+                    files.append(file_stat)
+        except (PermissionError, FileNotFoundError):
+            pass
+    return sorted(doubles)
+
 
 if __name__ == '__main__':
-    pass
+
+    if len(sys.argv) != 2:
+        sys.exit('Вы не указали путь')
+    root_path = sys.argv[1]
+
+    print(get_doubles(root_path))
