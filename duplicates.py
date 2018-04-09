d948738d4fe4a2828cfe1fe6cc21b6b2d3bc9ed3
Ubuntu <ubuntu@ip-172-31-44-106.us-east-2.compute.internal>
2018-04-09 14:05:02 +0000

    Поправил по замечаниям

--- duplicates.py
+++ duplicates.py
@@ -7,21 +7,26 @@ def get_size(filepath):
     return os.stat(filepath)[6]
 
 
-def get_files(root_dir):
-    files = defaultdict(int)
-    for (dirpath, dirnames, filenames) in os.walk(root_dir):
+def get_files_data(root_dir):
+    files = defaultdict(list)
+    for dirpath, dirnames, filenames in os.walk(root_dir):
         try:
             for filename in filenames:
                 fullpath = os.path.join(dirpath, filename)
-                file_stat = (filename, get_size(fullpath))
-                files[file_stat] += 1
+                file_stat = (filename, (get_size(fullpath)))
+                files[file_stat].append(fullpath)
         except (FileNotFoundError, PermissionError):
             pass
     return files
 
 
-def get_duplicates(files):
-    return sorted({key: value for key, value in files.items() if value > 1})
+def get_duplicates(files_data):
+    return {k: v for k, v in files_data.items() if len(v) > 1}
+
+
+def prettify_data(files_data):
+    for k, v in files_data.items():
+        print('File:', k[0], ' Size:', k[1], ' Files:', v)
 
 
 if __name__ == '__main__':
@@ -30,6 +35,6 @@ if __name__ == '__main__':
         sys.exit('Вы не указали путь')
     root_path = sys.argv[1]
 
-    files_data = get_files(root_path)
+    files_data = get_files_data(root_path)
     duplicates = get_duplicates(files_data)
-    print(duplicates)
+    prettify_data(duplicates)
